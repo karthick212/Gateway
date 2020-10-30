41dbd17db30aa6fe45a33246c89cde3aa73604d8
unknown <karthick212>
2020-10-30 21:59:17 +0530

    rst

---
+++ precache-manifest.4061766ed0e5db7942957c00a69e18aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e700f87bdab48e41d6451e2785afb7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce701191579c0ebb55d2",
+    "url": "/static/css/2.ef2ffecb.chunk.css"
+  },
+  {
+    "revision": "e371a9fe994f6f887754",
+    "url": "/static/css/main.f6fd3d58.chunk.css"
+  },
+  {
+    "revision": "ce701191579c0ebb55d2",
+    "url": "/static/js/2.17ba47b5.chunk.js"
+  },
+  {
+    "revision": "4e3f8d4bad1db285aba0fceffdc7b967",
+    "url": "/static/js/2.17ba47b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e371a9fe994f6f887754",
+    "url": "/static/js/main.4a7f2184.chunk.js"
+  },
+  {
+    "revision": "7646288671f8458d76de",
+    "url": "/static/js/runtime-main.49627031.js"
+  }
+]);
